cbb32a2359181c5b9ca884bfc2fa226860ade945
jatosi <50878437+jatosi@users.noreply.github.com>
2021-10-20 11:44:13 +0800

    Update vip_analysis.js

--- vip_analysis.js
+++ vip_analysis.js
@@ -11,8 +11,14 @@ $(function() {
         topValue = .04 * topValue;
         $("#palyer-iframe").height(palyerIframeHeight),
         $("#play-box").css("top", topValue),
-        otherWebsiteUrl = playUrl()
-        //alert(otherWebsiteUrl)
+        otherWebsiteUrl = playUrl(),
+        $("#film-play-url").val(otherWebsiteUrl),
+        $("#film-play-url-mobile").val(otherWebsiteUrl),
+        url = "https://vip.parwix.com:4433/player/?url=" + otherWebsiteUrl,
+        $("#palyer-iframe").attr("src", url)
+        //alert($("#palyer-iframe").src)
+        //alert(url)
+        
     }),
     $("#play-btn").on("click", function() {
         var sv, otherWebsiteUrl = getCurrentVideoUrl();
@@ -31,43 +37,22 @@ function playUrl() {
     httpUrl = decodeURI(httpUrl);
     //alert(httpUrl);
     if (httpUrl.indexOf("?") > 0) {
-        httpUrl = httpUrl.substring(httpUrl.indexOf("?"));
+        //httpUrl = httpUrl.substring(httpUrl.indexOf("?"));
         //httpUrl = decodeURI(encodeURI(httpUrl));
         //httpUrl = encodeURI(httpUrl);
         httpUrl = unescape(httpUrl);
         //alert(httpUrl);
-        if (httpUrl.indexOf("&") > 0) {
-            var urlArr = httpUrl.split("&");
-            //alert(urlArr.length);
-            if (urlArr.length >= 1) {
-                //alert(urlArr[1]);
-                if (urlArr[0].toString().indexOf("url=") > 0) {
-                    url = urlArr[0].substring(urlArr[0].indexOf("?") + 5);
-                    //alert(url);
-                }
-                if (urlArr[1].toString().indexOf("ic=") > 0) {
-                    pic = urlArr[1].substring(4);
-                    //alert(pic);
-                } else {
-                    pic = "/images/load.gif";
-                }
-                if (urlArr[2].toString().indexOf("tle=") > 0) {
-                    title = urlArr[2].substring(6);
-                    title = title.substring(0,title.length-5);
-                    //alert(title);
-                } else {
-                    title = "";
-                }
-            }
-            //alert("test");
-        } else {
-            if (httpUrl.indexOf("url=") > 0) {
-                url = httpUrl.substring(httpUrl.indexOf("?") + 5);
-                //url = httpUrl.replace("url=","");
-                alert(url);
-                pic = "/images/load.gif";
-            }
-        }
+        //alert(httpUrl.indexOf("url="));
+        str = httpUrl.replace("https://www.xxooe.com/zz.html?url=","");
+        str = str.replace("https://xxooe.com/zz.html?url=","");
+        str = str.replace(ROOT+"?url=","");
+        //alert(httpUrl.replace("https://www.xxooe.com/zz.html?url=",""));
+        //zz = document.domain + "/zz.html?url=";
+        //alert(zz);
+        return str;
+        
+        //$("#film-play-url").val()=
+        //$urlMobile = $("#film-play-url-mobile")
     } else {
         alert("播放地址错误！");
     }
